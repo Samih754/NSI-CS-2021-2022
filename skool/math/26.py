3dfbf02338cfcfd6e798be02dcbfd653dd75c918
Samih754 <91627784+Samih754@users.noreply.github.com>
2021-10-09 18:33:32 +0200

    Add files via upload

---
+++ skool/math/26.py
@@ -0,0 +1,14 @@
+
+hyp = float(input("hyp; "))
+b = float(input("b; "))
+c = float(input("c; "))
+
+result = ""
+
+if hyp ** 2 == (c ** 2 + b ** 2):
+    result = True
+else:
+    result = False
+
+
+print(result)
